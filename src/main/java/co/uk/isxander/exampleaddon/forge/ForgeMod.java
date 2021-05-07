981a25211c72c887a1f4570da16c41f9ac87b9f6
isXander <43245524+isXander@users.noreply.github.com>
2021-05-07 18:17:41 +0100

    Realised I could put this in the main class

--- src/main/java/co/uk/isxander/exampleaddon/forge/ForgeMod.java
+++
@@ -1,8 +0,0 @@
-package co.uk.isxander.exampleaddon.forge;
-
-import co.uk.isxander.exampleaddon.ExampleAddon;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(modid = "evergreenexampleaddon", name = ExampleAddon.NAME, version = ExampleAddon.VERSION, dependencies = "required-after:evergreenhud")
-public class ForgeMod {
-}
